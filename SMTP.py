567a53cc2e8333196f2d65001b616e007814c09f
Eric Ndivo <j.ericndivo@gmail.com>
2023-06-28 16:10:29 +0300

    Potential Exceptions

--- SMTP.py
+++ SMTP.py
@@ -4,18 +4,21 @@ import time
 port = 465
 print("Simple SMTP server")
 time.sleep(1)
+
 sender_email = (input(str("Enter your Email Address:\n")))
 print("")
 time.sleep(1)
+
 receiver_email = (input(str("Enter Receiver Email:\n")))
 print("")
-context = ssl.create_default_context()
+
 sender_password = getpass.getpass(prompt="Enter Your Email Password:\n", stream=None)
 message = "file"
 print("")
 time.sleep(1)
 print("[Attempting to Login...]")
 try:
+    context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, receiver_email, message)
